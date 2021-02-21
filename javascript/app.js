4cb6955173e4686676463d13f857b8cbc12e2d41
Arun Jangra <arunjngra89@gmail.com>
2021-02-21 21:55:57 +0530

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,27 +1,28 @@
 //selecting items from doc
 
+
 let type = document.getElementById('type');
 let exp = document.getElementById('Expense');
-// let date = document.getElementById('date');
 let desc = document.getElementById('desc');
 let submit = document.getElementById('submit');
 
 let table = document.querySelector('.display-items');
 
-
 let total = document.querySelector('.total');
-console.log(total);
 
+//calling show item function
 showItems()
 
+//adding event listener to the submit button
 submit.addEventListener('click', addVal);
+//adding event listener to the item in the table
 table.addEventListener('click', deleteItem);
 
+//function for adding the value
 function addVal() {
 
     let typ = type.value;
     let ex = exp.value;
-    // let dat = date.value;
     let des = desc.value;
     if (typ, ex, des != '') {
         ind = localStorage.length++;
@@ -35,38 +36,45 @@ function addVal() {
     }
 }
 
+//show item function
 function showItems() {
     if (localStorage.length > 0) {
         let totalSum = 0;
-        for (let i = 0; i < localStorage.length; i++) {
-            const element = localStorage[i].split(',');
-            let ele = document.createElement('tr');
-            ele.className = 'table-item';
-            ele.id = i;
+        for (let i = 0; i < localStorage.length + 1; i++) {
+            console.log(i);
+            if (localStorage.hasOwnProperty(i)) {
+                const element = localStorage[i].split(',');
+                let ele = document.createElement('tr');
+                ele.className = 'table-item';
+                ele.id = i;
 
-            ele.innerHTML = `
-                        <td>${element[0].toLowerCase()}</td>
-                        <td>$${element[1]}</td>
-                        <td>${element[2].toLowerCase()}</td>
-                    <td><button class="delete">X</button></td>
-                        
-                            `;
+                ele.innerHTML = `
+                                <td>${element[0].toLowerCase()}</td>
+                                <td>$${element[1]}</td>
+                                <td>${element[2].toLowerCase()}</td>
+                                <td><button class="delete">X</button></td>
+                                
+                                    `;
 
-            table.appendChild(ele);
-            totalSum += parseInt(element[1]);
+                table.appendChild(ele);
+                totalSum += parseInt(element[1]);
+                console.log(ele.id);
+            }
         }
         total.innerHTML = `<h3>Total : $${totalSum}</h3>`;
     }
 }
 
+
+//function for deleting the item
 function deleteItem(e) {
     if (e.target.classList.contains('delete')) {
         let ele = e.target.parentElement.parentElement;
         console.log(ele);
         table.removeChild(ele);
         localStorage.removeItem(ele.id);
+        location.reload();
     }
-    location.reload();
 }
 
 
